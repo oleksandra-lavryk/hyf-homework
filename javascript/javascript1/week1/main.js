0a67dfb1b90a34eeb5e8943a06f1b3bd12f54ff4
oleksandra-lavryk <oleksandra.lavryk96@gmail.com>
2021-12-07 13:18:33 +0100

    hausey pricey feature added

--- javascript/javascript1/week1/main.js
+++ javascript/javascript1/week1/main.js
@@ -21,3 +21,25 @@ if (shouldShowResultInDogYears) {
     "Your dog will be " + dogYear + " years old in " + dogYearFuture + "."
   );
 }
+
+// Housey pricey (A house price estimator)
+let names = ["Peter", "Julia"];
+let houseWide = [8, 5];
+let houseHight = [10, 8];
+let houseDepth = [10, 11];
+let gardenSize = [100, 70];
+let housePrice = [2500000, 1000000];
+let actualHousePrice;
+
+for (let i = 0; i < names.length; i++) {
+  actualHousePrice =
+    houseWide[i] * houseHight[i] * houseDepth[i] * 2.5 * 1000 +
+    gardenSize[i] * 300;
+  if (actualHousePrice < housePrice[i]) {
+    console.log(
+      "Hey " + names[i] + ", sorry but You are paing too much for this house."
+    );
+  } else {
+    console.log("Hey " + names[i] + ", the price for the house is OK.");
+  }
+}
