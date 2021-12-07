830ef7e826d8dde6430fb88604942d3ea0390f3f
oleksandra-lavryk <oleksandra.lavryk96@gmail.com>
2021-12-07 12:46:16 +0100

    adding age-ify feature

---
+++ javascript/javascript1/week1/main.js
@@ -0,0 +1,5 @@
+// Age-ify (A future age calculator)
+let yearOfBirth = 1996;
+let yearFuture = 2027;
+let age = yearFuture - yearOfBirth;
+console.log("You will be " + age + " years old in " + yearFuture + ".");
