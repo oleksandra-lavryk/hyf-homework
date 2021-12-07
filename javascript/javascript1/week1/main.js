74e23bcd780bfc8492b1640223d2387fab23c218
oleksandra-lavryk <oleksandra.lavryk96@gmail.com>
2021-12-07 12:55:51 +0100

    goodboy-oldboy feature added

--- javascript/javascript1/week1/main.js
+++ javascript/javascript1/week1/main.js
@@ -3,3 +3,21 @@ let yearOfBirth = 1996;
 let yearFuture = 2027;
 let age = yearFuture - yearOfBirth;
 console.log("You will be " + age + " years old in " + yearFuture + ".");
+
+// Goodboy-Oldboy (A dog age calculator)
+let dogYearOfBirth = 2018;
+let dogYearFuture = 2027;
+let dogYear;
+let shouldShowResultInDogYears = true;
+
+if (shouldShowResultInDogYears) {
+  dogYear = (dogYearFuture - dogYearOfBirth) * 7;
+  console.log(
+    "Your dog will be " + dogYear + " human years old in " + dogYearFuture + "."
+  );
+} else {
+  dogYear = dogYearFuture - dogYearOfBirth;
+  console.log(
+    "Your dog will be " + dogYear + " years old in " + dogYearFuture + "."
+  );
+}
