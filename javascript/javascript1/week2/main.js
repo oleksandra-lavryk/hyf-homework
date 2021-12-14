4dd77c3ee19b0eb2e233ebd6654211518e3171dc
oleksandra-lavryk <oleksandra.lavryk96@gmail.com>
2021-12-14 15:49:54 +0100

    event application function added

--- javascript/javascript1/week2/main.js
+++ javascript/javascript1/week2/main.js
@@ -20,3 +20,27 @@ const fullname1 = getFullname("Benjamin", "Hughes", true, "male");
 const fullname2 = getFullname("Line", "Hansen", true, "female");
 
 console.log(fullname1 + "\n" + fullname2);
+
+// Event application
+const weekDays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+function getEventWeekday(daysToEvent) {
+  const todayFullDate = new Date();
+  const meetingDate = new Date(
+    todayFullDate.getFullYear(),
+    todayFullDate.getMonth(),
+    todayFullDate.getDate() + daysToEvent
+  );
+  return weekDays[meetingDate.getDay()];
+}
+
+console.log(getEventWeekday(5));
+console.log(getEventWeekday(2));
