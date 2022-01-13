43698193107387628eb5d2320604774b7b58a5f3
oleksandra-lavryk <oleksandra.lavryk96@gmail.com>
2022-01-13 12:59:18 +0100

    series duration of my life added

--- javascript/javascript1/week3/main.js
+++ javascript/javascript1/week3/main.js
@@ -37,3 +37,53 @@ const travelInformation = {
 
 const travelTime = getTravelTime(travelInformation);
 console.log(travelTime); // 8 hours and 38 minutes
+
+// Series duration of my life
+const seriesDurations = [
+  {
+    title: "Game of thrones",
+    days: 3,
+    hours: 1,
+    minutes: 0,
+  },
+  {
+    title: "Sopranos",
+    days: 3,
+    hours: 14,
+    minutes: 0,
+  },
+  {
+    title: "The Wire",
+    days: 2,
+    hours: 12,
+    minutes: 0,
+  },
+  {
+    title: "Bones",
+    days: 7,
+    hours: 3,
+    minutes: 0,
+  },
+];
+const lifeDuration = 80 * 365 * 24 * 60;
+let totalPersent = 0;
+function logOutSeriesText() {
+  for (let i = 0; i < seriesDurations.length; i++) {
+    let perscent = Number(
+      (
+        ((seriesDurations[i].days * 24 * 60 +
+          seriesDurations[i].hours * 60 +
+          seriesDurations[i].minutes) /
+          lifeDuration) *
+        100
+      ).toFixed(3)
+    );
+
+    console.log(
+      seriesDurations[i].title + " tooks " + perscent + "% of my life"
+    );
+    totalPersent += perscent;
+  }
+}
+logOutSeriesText();
+console.log("In total that is " + totalPersent + "% of my life");
