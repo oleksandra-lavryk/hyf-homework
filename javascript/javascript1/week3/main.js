eb4b15724981a2471c2c3d227cd7a85420c3413e
oleksandra-lavryk <oleksandra.lavryk96@gmail.com>
2022-01-13 12:28:26 +0100

    time function added

--- javascript/javascript1/week3/main.js
+++ javascript/javascript1/week3/main.js
@@ -17,3 +17,23 @@ for (let i = 0; i < names.length; i++) {
   }
 }
 console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
+
+// When will we be there??
+function getTravelTime(travelInfo) {
+  let travelTimeResult = travelInfo.destinationDistance / travelInfo.speed;
+  let travelTimeResultInMinutes =
+    travelTimeResult - Math.trunc(travelTimeResult);
+  return (
+    Math.trunc(travelTimeResult) +
+    " hours " +
+    Math.trunc(travelTimeResultInMinutes * 60) +
+    " minutes"
+  );
+}
+const travelInformation = {
+  speed: 50,
+  destinationDistance: 432,
+};
+
+const travelTime = getTravelTime(travelInformation);
+console.log(travelTime); // 8 hours and 38 minutes
