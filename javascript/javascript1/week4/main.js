354f5c90feef40c79c5aa9761ecf36fa50137622
oleksandra-lavryk <oleksandra.lavryk96@gmail.com>
2022-01-19 13:42:10 +0100

    todo list features were added

--- javascript/javascript1/week4/main.js
+++ javascript/javascript1/week4/main.js
@@ -1,8 +1,14 @@
 let userName = "";
+let toDoList = new Set();
 
 function getReply(command) {
   const checkNameStr = "Hello my name is";
   const sayNameStr = "What is my name";
+  const toDoListAddStr = " to my todo list";
+  const toDoListRemoveStr = " from my todo list";
+  const checkAddStr = "Add ";
+  const checkRemoveStr = "Remove ";
+  const printToDoStr = "What is on my todo?";
 
   if (command.indexOf(checkNameStr) !== -1) {
     if (userName.length == 0) {
@@ -20,9 +26,50 @@ function getReply(command) {
       console.log("Your name is " + userName);
     }
   }
+
+  if (
+    command.indexOf(toDoListAddStr) !== -1 &&
+    command.indexOf(checkAddStr) !== -1
+  ) {
+    let todoItem = command;
+    todoItem = todoItem
+      .replaceAll(checkAddStr, "")
+      .replaceAll(toDoListAddStr, "");
+    toDoList.add(todoItem);
+  }
+
+  if (
+    command.indexOf(toDoListRemoveStr) !== -1 &&
+    command.indexOf(checkRemoveStr) !== -1
+  ) {
+    let todoItem = command;
+    todoItem = todoItem
+      .replaceAll(checkRemoveStr, "")
+      .replaceAll(toDoListRemoveStr, "");
+
+    if (toDoList.delete(todoItem)) {
+      console.log(todoItem + " removed from todo list.");
+    } else {
+      console.log("This item was not founded in todo list.");
+    }
+  }
+
+  if (command.indexOf(printToDoStr) !== -1) {
+    let printedToDoList = "ToDo list: ";
+    for (let item of toDoList) {
+      printedToDoList = printedToDoList + item + " / ";
+    }
+    console.log(printedToDoList);
+  }
 }
 
-getReply("Hello my name is Thomas");
-getReply("Hello my name is Thomas");
+// getReply("Hello my name is Thomas");
+// getReply("Hello my name is Thomas");
 
-getReply("What is my name");
+// getReply("What is my name");
+
+getReply("Add fishing to my todo list");
+getReply("Add reading to my todo list");
+getReply("Add singing in the shower to my todo list");
+getReply("Remove reading from my todo list");
+getReply("What is on my todo?");
