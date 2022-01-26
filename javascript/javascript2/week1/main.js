666861eea053f561ac4d50c15f68d217c700b00e
oleksandra-lavryk <oleksandra.lavryk96@gmail.com>
2022-01-26 16:55:22 +0100

    hyfBay fixed

--- javascript/javascript2/week1/main.js
+++ javascript/javascript2/week1/main.js
@@ -3,9 +3,9 @@ console.log("Script loaded");
 const products = getAvailableProducts();
 console.log(products);
 
-const priductUl = document.createElement("ul");
-priductUl.classList.add("products-list");
-document.querySelector("main").appendChild(priductUl);
+const productUl = document.createElement("ul");
+productUl.classList.add("products-list");
+document.querySelector("main").appendChild(productUl);
 
 function renderProducts(products) {
   for (let i = 0; i < products.length; i++) {
@@ -19,7 +19,7 @@ function renderProducts(products) {
       products[i].rating +
       "</span>";
 
-    priductUl.appendChild(productLi);
+    productUl.appendChild(productLi);
   }
 }
 renderProducts(products);
