7366e0c6ea46409d66361afeb604af83bbf96795
oleksandra-lavryk <oleksandra.lavryk96@gmail.com>
2022-01-26 15:30:09 +0100

    hyfBay li adding is done

--- javascript/javascript2/week1/main.js
+++ javascript/javascript2/week1/main.js
@@ -2,3 +2,22 @@ console.log("Script loaded");
 
 const products = getAvailableProducts();
 console.log(products);
+
+const priductUl = document.querySelector(".products-list");
+
+function renderProducts(products) {
+  for (let i = 0; i < products.length; i++) {
+    let productLi = document.createElement("li");
+    productLi.innerHTML =
+      "<h2>" +
+      products[i].name +
+      "</h2><span> price: " +
+      products[i].price +
+      "</span><span> rating: " +
+      products[i].rating +
+      "</span>";
+
+    priductUl.appendChild(productLi);
+  }
+}
+renderProducts(products);
