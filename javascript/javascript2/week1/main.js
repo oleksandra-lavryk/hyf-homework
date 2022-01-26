078651b1af1744ab2181a599d6b307cf68717e26
oleksandra-lavryk <oleksandra.lavryk96@gmail.com>
2022-01-26 08:56:01 +0100

    shortest word function added

--- javascript/javascript2/week1/main.js
+++ javascript/javascript2/week1/main.js
@@ -1,2 +1,16 @@
 const danishWords = ["bil", "plante", "kaffe", "bog", "ø", "planetarium"];
-getShortestWord(danishWords); // returns 'ø'
+
+function getShortestWord(wordsArr) {
+  if (wordsArr.length == 0) {
+    return "Words not found!";
+  }
+  let shortWord = wordsArr[0];
+  let shortWordLength = wordsArr[0].length;
+  for (let i = 0; i < wordsArr.length; i++) {
+    if (wordsArr[i].length < shortWordLength) {
+      shortWord = wordsArr[i];
+    }
+  }
+  return shortWord;
+}
+console.log(getShortestWord(danishWords)); // returns 'ø'
