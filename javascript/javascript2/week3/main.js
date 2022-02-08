35c4cd1a50aa40913bd9c454962ae63467c3ade0
oleksandra-lavryk <oleksandra.lavryk96@gmail.com>
2022-02-08 11:23:13 +0100

    comments with tasks adede

--- javascript/javascript2/week3/main.js
+++ javascript/javascript2/week3/main.js
@@ -1,16 +1,29 @@
-// setTimeout(() => console.log("This string logged after 2.5 seconds."), 2500);
+// Log out the text Called after 2.5 seconds 2.5 seconds after the script is loaded.
 
-// function printWithDelay(delay, strToPrint) {
-//   setTimeout(() => console.log(strToPrint), delay * 1000);
-// }
+setTimeout(() => console.log("This string logged after 2.5 seconds."), 2500);
 
-// printWithDelay(4, "This string logged after 5 seconds.");
-// printWithDelay(1, "This string logged after 1 seconds.");
+// Create a function that takes 2 parameters: delay and stringToLog. Calling this function should log out the stringToLog after delay seconds.
+// Call the function you have created with some different arguments.
 
-// const printBtn = document.querySelector(".print-button");
-// printBtn.addEventListener("click", () =>
-//   printWithDelay(5, "printed this text with 5s delay after button was clicked")
-// );
+function printWithDelay(delay, strToPrint) {
+  setTimeout(() => console.log(strToPrint), delay * 1000);
+}
+
+printWithDelay(4, "This string logged after 5 seconds.");
+printWithDelay(1, "This string logged after 1 seconds.");
+
+// Create a button in html. When clicking this button, use the function
+// you created in the previous task to log out the text: Called after 5 seconds 5 seconds after the button is clicked.
+
+const printBtn = document.querySelector(".print-button");
+printBtn.addEventListener("click", () =>
+  printWithDelay(5, "printed this text with 5s delay after button was clicked")
+);
+
+// Create two functions and assign them to two different variables.
+// One function logs out Earth, the other function logs out Saturn.
+// Now create a new third function that has one parameter: planetLogFunction.
+// The only thing the third function should do is call the provided parameter function.
 
 const earthLogger = () => console.log("Earth");
 const saturnLogger = () => console.log("Saturn");
@@ -20,6 +33,10 @@ function planetLogFunction(func) {
 }
 planetLogFunction(earthLogger);
 planetLogFunction(saturnLogger);
+
+// Create a button with the text called "Log location".
+// When this button is clicked the location (latitude, longitude) of the user should be logged out using this browser api
+
 let currentLatitude;
 let currentLongitude;
 function logLocation() {
@@ -41,6 +58,7 @@ function logLocation() {
 const locationBtn = document.querySelector(".location-button");
 locationBtn.addEventListener("click", logLocation);
 
+// Show current location  on a map using  the Google maps api
 let map;
 
 function initMap() {
@@ -49,13 +67,21 @@ function initMap() {
     zoom: 12,
   });
 }
+// Create a function called runAfterDelay. It has two parameters: delay and callback.
+// When called the function should wait delay seconds and then call the provided callback function.
 
 function runAfterDelay(delay, callback) {
   setTimeout(callback, delay * 1000);
 }
 runAfterDelay(5, () => console.log("printed with delay in 5 seconds"));
 
+// Check if we have double clicked on the page. A double click is defined by two clicks within 0.5 seconds.
+// If a double click has been detected, log out the text: "double click!"
+
 document.addEventListener("dblclick", () => console.log("double click!"));
+
+// Create a function called jokeCreator that has three parameters: shouldTellFunnyJoke - boolean, logFunnyJoke - function and logBadJoke - function.
+// If you set shouldTellFunnyJoke to true it should call the logFunnyJoke function that should log out a funny joke. And vice versa.
 
 function jokeCreator(shouldTellFunnyJoke, logFunnyJoke, logBadJoke) {
   if (shouldTellFunnyJoke) {
@@ -75,3 +101,13 @@ jokeCreator(
   () => console.log("Funny joke is here:)"),
   () => console.log("Bad joke is here :(")
 );
+
+// Create an array with 3 items. All items should be functions. Iterate through the array and call all the functions.
+
+const functionArray = [
+  () => console.log("first array function"),
+  () => setTimeout(() => console.log("second array function"), 3000),
+  () => console.log("third array function"),
+];
+
+functionArray.forEach((funct) => funct());
