92fb39511f7b1d7bdc98f71f4c5025c52170969d
oleksandra-lavryk <oleksandra.lavryk96@gmail.com>
2022-02-16 14:35:23 +0100

    gif app added

---
+++ javascript/javascript3/week1/giphyApp/main.js
@@ -0,0 +1,35 @@
+const searchBtn = document.querySelector("button");
+const wordInput = document.getElementById("search-word");
+const wordAmountInput = document.getElementById("gif-amount");
+const gifContainer = document.getElementById("gif-result-conatiner");
+const apiKey = "dr8wkOEpX1Ws8a5AdjQdA8iBQrZumR3q";
+
+searchBtn.addEventListener("click", () => {
+  if (wordInput === "") {
+    alert("Input a word");
+  } else {
+    (async function () {
+      const response = await fetch(
+        `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${wordInput.value}`
+      );
+      if (response.ok) {
+        const content = await response.json();
+        renderGifs(content.data);
+      }
+    })();
+  }
+});
+
+function renderGifs(gifArr) {
+  gifContainer.innerHTML = "";
+  if (wordAmountInput.value > 0) {
+    gifArr = gifArr.slice(0, wordAmountInput.value);
+  }
+  gifArr.forEach((gifItem) => {
+    const gifDiv = document.createElement("div");
+    const gifImg = document.createElement("img");
+    gifImg.src = gifItem.images.downsized.url;
+    gifDiv.appendChild(gifImg);
+    gifContainer.appendChild(gifDiv);
+  });
+}
---
+++ javascript/javascript3/week1/weatherApp/main.js
@@ -0,0 +1,91 @@
+const getCityBtn = document.getElementById("show-by-city");
+const byPositionBtn = document.getElementById("show-by-position");
+const cityInput = document.querySelector("input");
+const weatherIcon = document.getElementById("weather-icon");
+const resultBlock = document.querySelector(".weather-result-container");
+const showMap = document.getElementById("show-map");
+let map;
+
+getCityBtn.addEventListener("click", () => {
+  resultBlock.style.opacity = "0";
+  document.getElementById("map-container").style.opacity = "0";
+  const cityValue = cityInput.value;
+  if (cityValue === "") {
+    alert("Please input city name!");
+  } else {
+    (async function () {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=5a9f05de702d708299f07803e54ed74d&units=metric`
+      );
+      if (response.ok) {
+        const content = await response.json();
+        showWeather(content);
+      } else {
+        alert("Sorry, information not found!");
+      }
+    })();
+  }
+});
+
+byPositionBtn.addEventListener("click", () => {
+  resultBlock.style.opacity = "0";
+  document.getElementById("map-container").style.opacity = "0";
+  navigator.geolocation.getCurrentPosition(
+    (currentPosition) => {
+      const currentLatitude = currentPosition.coords.latitude;
+      const currentLongitude = currentPosition.coords.longitude;
+      (async function () {
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${currentLatitude}&lon=${currentLongitude}&appid=5a9f05de702d708299f07803e54ed74d&units=metric`
+        );
+        if (response.ok) {
+          const content = await response.json();
+          showWeather(content);
+        } else {
+          alert("Sorry, information not found!");
+        }
+      })();
+      initMap(currentLongitude, currentLatitude);
+    },
+    (error) => alert(error.message)
+  );
+});
+
+function showWeather(content) {
+  document.getElementById("city-name").innerText = content.name;
+  let sunRiseDate = new Date(content.sys.sunrise * 1000);
+  let sunSetDate = new Date(content.sys.sunset * 1000);
+
+  document.getElementById("today-date").innerText =
+    sunRiseDate.toLocaleDateString();
+
+  weatherIcon.src = `http://openweathermap.org/img/wn/${content.weather[0].icon}@2x.png`;
+
+  document.getElementById("current-temp").innerHTML = `${parseInt(
+    content.main.temp
+  )} &#x2103;`;
+
+  document.getElementById("wind-speed").innerText = `Wind: ${parseInt(
+    content.wind.speed
+  )}m/s`;
+  document.getElementById(
+    "clouds"
+  ).innerText = `Clouds: ${content.clouds.all}%`;
+  document.getElementById(
+    "sun-rise"
+  ).innerText = `Sunrise at: ${sunRiseDate.toLocaleTimeString()}`;
+  document.getElementById(
+    "sun-set"
+  ).innerText = `Sunrset at: ${sunSetDate.toLocaleTimeString()}`;
+  resultBlock.style.opacity = "1";
+  if (showMap.checked) {
+    initMap(content.coord.lon, content.coord.lat);
+  }
+}
+function initMap(lon, lat) {
+  map = new google.maps.Map(document.getElementById("map-container"), {
+    center: { lat: lat, lng: lon },
+    zoom: 12,
+  });
+  document.getElementById("map-container").style.opacity = "1";
+}
