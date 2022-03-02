9641d6ae285e670048e116da92021c1601be6936
oleksandra-lavryk <oleksandra.lavryk96@gmail.com>
2022-03-02 15:11:51 +0100

    repos promise all added

---
+++ javascript/javascript3/week3/promises_repo/main.js
@@ -0,0 +1,35 @@
+// Fetch all the 3 classmates repositories at the same time using Promise.all
+
+function printReposOnPage(reposArr) {
+  reposArr.forEach((element) => {
+    if (element.items.length) {
+      const repoLi = document.createElement("li");
+      const repoTitle = document.createElement("h2");
+      repoTitle.innerText = element.items[0].owner.login;
+      repoLi.appendChild(repoTitle);
+      element.items.forEach((repo) => {
+        const repoSubItem = document.createElement("p");
+        repoSubItem.innerHTML = `
+        <span>${repo.full_name}: </span><span> ${repo.url} </span>
+        `;
+        repoLi.appendChild(repoSubItem);
+      });
+      document.querySelector(".repo-container").appendChild(repoLi);
+    }
+  });
+}
+
+Promise.all([
+  fetch("https://api.github.com/search/repositories?q=user:rismita87"),
+  fetch("https://api.github.com/search/repositories?q=user:sergii-borodin"),
+  fetch("https://api.github.com/search/repositories?q=user:SvitlanaBurlaka"),
+])
+  .then((responses) => {
+    return Promise.all(
+      responses.map((response) => {
+        return response.json();
+      })
+    );
+  })
+  .then((data) => printReposOnPage(data))
+  .catch((error) => console.log(error));
