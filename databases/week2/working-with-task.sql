73d95e86dde9b08d48ecf7c74edc222ba3029362
oleksandra-lavryk <oleksandra.lavryk96@gmail.com>
2022-03-15 10:31:30 +0100

    task database more queries added

--- databases/week2/working-with-task.sql
+++ databases/week2/working-with-task.sql
@@ -44,3 +44,55 @@ FROM
   task
 WHERE
   id = 36;
+
+-- Get all the tasks assigned to users whose email ends in @spotify.com
+SELECT
+  *
+FROM
+  task
+JOIN
+  user
+ON
+  task.user_id = user.id
+WHERE
+  user.email LIKE "%@spotify.com";
+
+-- Get all the tasks for 'Donald Duck' with status 'Not started'
+SELECT
+ *
+FROM
+  task
+JOIN
+  user
+ON
+  task.user_id = user.id
+JOIN
+  status
+ON
+  task.status_id = status.id
+WHERE
+  user.name LIKE "Donald Duck"
+AND
+  status.name LIKE "Not started";
+
+-- Get all the tasks for 'Maryrose Meadows' that were created in september (hint: month(created)=month_number)
+SELECT
+ *
+FROM
+  task
+JOIN
+  user
+ON
+  task.user_id = user.id
+WHERE
+  user.name LIKE "Maryrose Meadows"
+AND
+  month(task.created) = 9;
+
+-- Find how many tasks where created in each month, e.g. how many tasks were created in october,
+--  how many tasks were created in november, etc. (hint: use group by)
+SELECT
+  month(task.created) creating_month, COUNT(task.id) as task_amount
+FROM
+  task
+GROUP BY  creating_month;  
