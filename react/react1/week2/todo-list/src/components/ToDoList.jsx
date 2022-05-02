749a5d68ff3e3d0b1cfd8f2a399e4188d9a76e0c
oleksandra-lavryk <oleksandra.lavryk96@gmail.com>
2022-05-02 20:26:35 +0200

    add random task and delete added

--- react/react1/week2/todo-list/src/components/ToDoList.jsx
+++ react/react1/week2/todo-list/src/components/ToDoList.jsx
@@ -1,18 +1,57 @@
-import tasklist from "../data/tasklist";
 import TaskItem from "./TaskItem";
+import tasklist from "../data/tasklist";
+import { useState, useEffect } from "react";
 
 export default function ToDoList() {
+  const [items, setItems] = useState(tasklist);
+
+  function addItem() {
+    setItems((prevItems) => {
+      return [
+        ...prevItems,
+        {
+          id: new Date().getTime(),
+          description: "Random task ",
+          deadline: "Date",
+        },
+      ];
+    });
+  }
+
+  function deleteItem(idToDelete) {
+    setItems((prevItems) => {
+      return prevItems.filter((item) => item.id != idToDelete);
+    });
+  }
+
+  function DeleteButton(props) {
+    return <button onClick={() => deleteItem(props.forDelete)}>Delete</button>;
+  }
+
+  // if (items.length === 0) {
+  //   return <p>No items in toDo list</p>;
+  // } else {
+
   return (
-    <ul class="list">
-      {tasklist.map((item) => {
-        return (
-          <TaskItem
-            key={item.id}
-            description={item.description}
-            deadline={item.deadline}
-          />
-        );
-      })}
-    </ul>
+    <>
+      <button onClick={() => addItem()}>Add random task</button>
+      <ul className="list">
+        {items.length ? (
+          items.map((item) => {
+            return (
+              <li key={item.id} className="list-item">
+                <TaskItem
+                  description={item.description}
+                  deadline={item.deadline}
+                />
+                <DeleteButton forDelete={item.id} />
+              </li>
+            );
+          })
+        ) : (
+          <p>No items</p>
+        )}
+      </ul>
+    </>
   );
 }
